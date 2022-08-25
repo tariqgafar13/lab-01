16ba3a9f3938e0ca8074144eded1ddad4d5e25a7
tariqgafar13 <112101368+tariqgafar13@users.noreply.github.com>
2022-08-25 10:51:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/************************************************************************
+* Hello.java
+*************************************************************************
+* Tariq Gafar
+* 08/24/2022
+* 255001
+************************************************************************/
+
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Tariq Gafar");
+		System.out.println("macOS x64");
+		System.out.println("Lab 01-Instructions Mac.");
+		System.out.println("255-001");
+		System.out.println();
+	
+	}
+}
